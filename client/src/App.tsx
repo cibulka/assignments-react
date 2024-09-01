1260d735d2b9fcb5460780d9730cc03d8f89da2a
Petr Cibulka <cibulka.me@gmail.com>
2024-09-01 15:03:11 +0200

    feat: [F8] calculate items per their state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,17 +6,26 @@ import { Footer } from "./components/Footer";
 import { ThemeProvider } from "./components/providers/ThemeProvider";
 import { ItemsProvider } from "./components/providers/items-provider";
 import { useItemsContext, useOnItemAddOrEdit } from "./components/providers/items-provider/hooks";
+import { useMemo } from "react";
 
 const AppProvided = () => {
-    const { state: { edited } } = useItemsContext();
+    const { state: { edited, items } } = useItemsContext();
+    
     const onItemAddOrEdit = useOnItemAddOrEdit(edited?.id || null);
+
+    const [doneItems, todoItems] = useMemo(() => {
+        return [
+            items.reduce((previous, current) => previous + (current.isDone ? 0 : 1), 0),
+            items.reduce((previous, current) => previous + (current.isDone ? 1 : 0), 0),
+        ];
+    }, [items]);
 
     return (
         <Container>
             <Layout>
                 <Header onItemAdd={onItemAddOrEdit}>To Do app</Header>
                 <List />
-                <Footer />
+                <Footer doneItems={doneItems} todoItems={todoItems}  />
             </Layout>
         </Container>
     );
