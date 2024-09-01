0cb0a524bdfc6e1c5c64503801ae16f459bc47a4
Petr Cibulka <cibulka.me@gmail.com>
2024-09-01 14:59:10 +0200

    feat: [F7] sort todo items

--- client/src/components/List.tsx
+++ client/src/components/List.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 import { useGetOnItemDelete, useGetOnItemDoneToggle, useGetOnItemLabelEdit, useItemsContext } from "./providers/items-provider/hooks";
 import { ListItem } from "./ListItem";
+import { useMemo } from "react";
 
 export const ListStyled = styled.div`
     display: flex;
@@ -10,18 +11,29 @@ export const ListStyled = styled.div`
 `;
 
 export const List = () => {
-    const { state } = useItemsContext();
+    const { state: { items } } = useItemsContext();
 
     const getOnItemDoneToggle = useGetOnItemDoneToggle();
     const getOnItemDelete = useGetOnItemDelete();
     const getOnItemLabelEdit = useGetOnItemLabelEdit();
+
+    const itemsSorted = useMemo(() => {
+        return [...items].sort((a, b) => {
+            if (!a.isDone && b.isDone) {
+                return -1;
+            } else if (a.isDone && !b.isDone) {
+                return 1;
+            }
+            return b.createdAt - a.createdAt;
+        });
+    }, [items]);
 
     return (
         <ListStyled>
             {/* TODO: Implement error view */}
             {/* TODO: Implement empty view */}
             {/* TODO: Implement loading view */}
-            {state.items.map(item => {
+            {itemsSorted.map(item => {
                 const onItemDelete = getOnItemDelete(item.id);
                 const onItemLabelEdit = getOnItemLabelEdit(item.id);
                 const onItemDoneToggle = getOnItemDoneToggle(item.id);
